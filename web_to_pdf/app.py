d1114bd288799dd5ac19c7c3beded9496372e35a
nag2mani <nag2mani@gmail.com>
2024-08-28 09:50:38 +0530

    added web to pdf

---
+++ web_to_pdf/app.py
@@ -0,0 +1,19 @@
+import requests
+from weasyprint import HTML
+
+def save_webpage_as_pdf(url, output_filename):
+    # Fetch the HTML content from the URL
+    response = requests.get(url)
+    html_content = response.content.decode('utf-8')
+
+    # Convert HTML to PDF
+    html = HTML(string=html_content, base_url=url)
+    html.write_pdf(output_filename)
+
+if __name__ == "__main__":
+    url = "https://en.wikipedia.org/wiki/Alfred_Nobel"
+    output_filename = "output.pdf"
+
+    save_webpage_as_pdf(url, output_filename)
+
+
