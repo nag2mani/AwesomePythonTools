85bb862455b6989e64ec05578fa03f23d5e4f040
Nagmani kumar <nag2mani@gmail.com>
2024-09-28 17:38:50 +0530

    added chrome refresher

---
+++ auto_refresher/app.py
@@ -0,0 +1,39 @@
+# Python Library
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Chrome
+options = Options()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.add_argument("--profile-directory=Default")
+options.add_argument("--user-data-dir=/var/tmp/chrome_user_data")  # Change this to your custom user data directory.
+
+# Disable webdriver_manager logs
+os.environ["WDM_LOG_LEVEL"] = "0"
+
+# Initialize the Chrome driver using the Service object
+driver_service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=driver_service, options=options)
+
+# Open the desired page
+driver.get('https://github.com/nag2mani')
+
+# Refresh the page every 2 seconds
+a=1
+
+try:
+    while True:
+        time.sleep(2)
+        driver.refresh()
+        print(f"Page refreshed {a} times")
+        a=a+1
+except KeyboardInterrupt:
+    print("Stopped by user.")
+    driver.quit()
+
+
+
